0dd90a4a0eb1da13bab8971765d91ca63b925df5
Shehryar_Kiyani <shehryar@xenex-media.com.au>
2023-12-23 19:55:44 +0500

    error handling files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import dotenv from "dotenv"
 import ConnectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({path:"./env"})
-ConnectDB();
+
+ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server running at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed")
+})
