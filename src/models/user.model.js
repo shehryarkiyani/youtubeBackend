0e913b1c4ab61189e152be96bd997219e9a4f79f
Shehryar_Kiyani <shehryar@xenex-media.com.au>
2023-12-26 12:49:30 +0500

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,85 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+const Userschema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+Userschema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+Userschema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+Userschema.methods.generateAccessToken = function(){
+  return  jwt.sign(
+        {
+        _id:this._id,
+        email:this._email,
+        username:this._username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+Userschema.methods.generateRefresToken = function(){
+    return  jwt.sign(
+        {
+        _id:this._id,
+        email:this._email,
+      
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
+export const User = mongoose.model("User", Userschema);
